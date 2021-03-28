b44ba06a385cafbfb45097a4af3d6a4581a19363
jhscavalcante <henrique.cavalcante@yahoo.com.br>
2021-03-28 10:33:56 -0400

    Done

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,41 @@
+import { ReactNode } from 'react';
+import ReactModal from 'react-modal';
+
+type ModalProps = {
+  setIsOpen: (isOpen: boolean) => void;
+  isOpen: boolean;
+  children: ReactNode;
+};
+
+const Modal = ({ setIsOpen, isOpen, children }: ModalProps) => {
+  return (
+    <ReactModal
+      shouldCloseOnOverlayClick={!false}
+      onRequestClose={() => setIsOpen(false)}
+      isOpen={isOpen}
+      ariaHideApp={false}
+      style={{
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          background: '#F0F0F5',
+          color: '#000000',
+          borderRadius: '8px',
+          width: '736px',
+          border: 'none',
+        },
+        overlay: {
+          backgroundColor: '#121214e6',
+        },
+      }}
+    >
+      {children}
+    </ReactModal>
+  );
+};
+
+export default Modal;
